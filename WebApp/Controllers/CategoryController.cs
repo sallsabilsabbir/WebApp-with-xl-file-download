5f6d0b76a603c4523d82d78f02da66e3fc010fbc
scube <scube@SCUBE>
2022-06-09 17:07:29 +0600

    Add project files.

---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,149 @@
+﻿using Fingers10.ExcelExport.ActionResults;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cant not exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        // ##############  For Edit ##############//   start
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                /*TempData["success"] = "Category updated successfully";*/
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        // ############################################################// Edit   End
+
+
+        // ##############  For Delete ##############//    start
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // ############################################################// Delete   End
+
+
+
+
+        // ################ Generate files ############################ start
+
+
+        /*DownloadExcel start */
+
+        public IActionResult DownloadExcel()
+        {
+           
+
+            IEnumerable<Category> objCategoryList = _db.Categories;
+
+            return new ExcelResult<Category>(objCategoryList, "sheet1","file");
+
+        }
+        /*DownloadExcel end*/
+
+    }
+}
+
---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using Fingers10.ExcelExport.Attributes;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [IncludeInReport(Order =1)]
+        [Key]
+        public int Id { get; set; }
+        [IncludeInReport(Order = 2)]
+        [Required]
+        public string Name { get; set; }
+        [IncludeInReport(Order = 3)]
+        [DisplayName("Display Order")]
+        [Range(1, 500, ErrorMessage = "Display Order must be between 1 and 500 only!!")]
+        public int DisplayOrder { get; set; }
+        [IncludeInReport(Order = 4)]
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
